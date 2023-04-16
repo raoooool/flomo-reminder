18a3ced6598e87932bea3c059bbd036a2f4d4703
Jun Wen <raool@foxmail.com>
2023-04-16 09:12:01 +0800

    feat: 支持推送 tags 配置

--- service/EnvService.ts
+++ service/EnvService.ts
@@ -1,13 +1,22 @@
 class EnvService {
   envs = {
-    FLOMO_AUTHORIZATION: process.env.FLOMO_AUTHORIZATION || "",
-    BARK_TOKEN: process.env.BARK_TOKEN || "",
+    FLOMO_AUTHORIZATION: process.env.FLOMO_AUTHORIZATION || "", // flomo jwt
+    BARK_TOKEN: process.env.BARK_TOKEN || "", // bark token
+    PUSH_TAGS: process.env.PUSH_TAGS || "", // 需要推送的 tags，用逗号分割
   };
+
+  requiredEnvs: (keyof typeof this.envs)[] = [
+    "FLOMO_AUTHORIZATION",
+    "BARK_TOKEN",
+  ];
 
   constructor() {
     const lackEnvs = (
       Object.keys(this.envs) as Array<keyof typeof this.envs>
     ).filter((key) => {
+      if (!this.requiredEnvs.includes(key)) {
+        return false;
+      }
       return !this.envs[key];
     });
 
--- service/MemoService.ts
+++ service/MemoService.ts
@@ -1,15 +1,30 @@
 import { random } from "lodash";
 import { Memo } from "../types";
 import { convert } from "html-to-text";
+import envService from "./EnvService";
 
 export class MemoService {
+  private pushTags = envService.envs.PUSH_TAGS.split(",");
+
   private convertMemoToHtml(memo: Memo) {
     return convert(memo?.content || "", {
       wordwrap: false,
     });
   }
 
+  private getMemo(memos: Memo[]) {
+    return this.convertMemoToHtml(memos[random(memos.length - 1)]);
+  }
+
   getLuckyMemo(memos: Memo[]) {
-    return this.convertMemoToHtml(memos[random(memos.length)]);
+    const tagMemos = memos.filter((memo) =>
+      this.pushTags.some((tag) =>
+        memo.tags.some((memoTag) => memoTag.includes(tag))
+      )
+    );
+    if (tagMemos.length) {
+      return this.getMemo(tagMemos);
+    }
+    return this.getMemo(memos);
   }
 }
